073fb1ff5a42d3fec88ae4cb5c95b0ab59e8785f
omuwa-ileile <omuwa.ileile@pau.edu.ng>
2025-10-08 23:14:25 +0100

    Add files via upload

---
+++ practice_11.rs
@@ -0,0 +1,13 @@
+fn main(){
+	printin!("Welcome to COS 101!");
+
+	printin!("The course learning outcome is as follows:");
+    
+    printin!("1. Distinguish between computer and computer programming,/n
+    	      2. Develop some techniques in computer science,/n
+    	      3. Understand the difference areas of study in computer sciene,/n 
+    	      4. Conversant with applications of computer science,/n 
+    	      5. Navigate the career prospects in computer science,/n 
+    	      6. Conversant with computer programming concepts.");
+
+}
