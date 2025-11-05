2427197d89afb6146cf5c52d73248468f3d62cf5
OMUWA <omuwa.ileile@pau.edu.ng>
2025-11-05 17:53:27 +0100

    Add week5 to github

--- week-4/project_1/src/main.rs
+++ week-4/project_1/src/main.rs
@@ -1,5 +1,4 @@
 use std::io;
-use std::f64;
 
 fn main() {
     println!("Calculate the roots of a quadratic equation");
@@ -7,30 +6,31 @@ fn main() {
     println!("Enter your value for a:");
     let mut input1 = String::new();
     io::stdin().read_line(&mut input1).expect("Not a valid string");
-    let a:f64 = input1.trim().parse().expect("Not a valid number");
+    let a: f64 = input1.trim().parse().expect("Not a valid number");
 
     println!("Enter your value for b:");
     let mut input2 = String::new();
     io::stdin().read_line(&mut input2).expect("Not a valid string");
-    let b:f64 = input2.trim().parse().expect("Not a valid number");
+    let b: f64 = input2.trim().parse().expect("Not a valid number");
 
     println!("Enter your value for c:");
     let mut input3 = String::new();
     io::stdin().read_line(&mut input3).expect("Not a valid string");
-    let c:f64 = input3.trim().parse().expect("Not a valid number");
+    let c: f64 = input3.trim().parse().expect("Not a valid number");
 
     let d = b * b - (4.0 * a * c);
     
     if d > 0.0{
         let root1 = (-b + d.sqrt()) / (2.0 * a);
         let root2 = (-b - d.sqrt()) / (2.0 * a);
-        println!("There are two distinct real roots which are: {} and {}", root1, root2);
+        println!("There are two distinct real roots which are: {:.2} and {:.2}", root1, root2);
     }
-   else if d == 0.0{
+    else if d == 0.0{
         let root = -b / (2.0 * a);
-        println!("There is one real root which is {}", root);
+        println!("There is one real root which is {:.2}", root);
     }
-    else {
+    else if d < 0.0 {
         println!("There are no real roots.");
     }
+    
 }
