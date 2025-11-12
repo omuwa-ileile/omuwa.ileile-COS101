8e876178d0bfe28808c25caae1e6bf47b0c096b1
OMUWA <omuwa.ileile@pau.edu.ng>
2025-11-12 17:09:57 +0100

    Functions and Arrays

---
+++ week-7/practice9/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+    let arr:[i32;4] = [10,20,30,40];
+    println!("Array is {:?}",arr);
+    println!("Array size is: {}",arr.len());
+
+    for val in arr.iter(){
+        println!("Value is: {}",val);
+    }
+}
